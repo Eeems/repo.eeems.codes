b96877a5caa08813b33be09f4ab7fa3d63814a08
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2023-01-15 11:48:42 -0700

    Add some dependencies and also mirror web files

--- scripts/mirror.sh
+++ scripts/mirror.sh
@@ -11,9 +11,11 @@ set -e
 log(){ echo -e "\033[0;31m==> $@\033[0m"; }
 log "Setting up..."
 sudo mkdir -p tmp
-sudo chown notroot:notroot tmp
-echo "$SSH_KEY" > tmp/server_key
+sudo chown notroot:notroot tmp repo www
+touch tmp/server_key
 chmod 600 tmp/server_key
+echo "$SSH_KEY" > tmp/server_key
+cp -r www/* repo/
 log "Uploading to $SERVER..."
 rsync -Pcuav --delete -e "ssh -p 22 -oStrictHostKeyChecking=no -i tmp/server_key" repo/* "$USER@$SERVER:$DIR/staging"
 ssh -p 22 -oStrictHostKeyChecking=no -i tmp/server_key "$USER@$SERVER" "rsync -a --delete --link-dest='$DIR/staging' '$DIR/staging/'* '$DIR/live'"
